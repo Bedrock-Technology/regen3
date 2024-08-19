1944cb042bc095a50bd969fb341a7e7f06f96ed9
MarcusBright <maryhasapoorlamb@hotmail.com>
2024-08-19 11:34:18 +0800

    a

--- scanner/reportSpec.go
+++ scanner/reportSpec.go
@@ -15,8 +15,10 @@ type ReportSpec struct {
 }
 
 type Transactions struct {
-	Total uint64
-	Incr  uint64
+	Total    uint64
+	Incr     uint64
+	TotalFee string
+	IncrFee  string
 }
 
 func (r *ReportSpec) Run() {
@@ -32,7 +34,13 @@ func (r *ReportSpec) Run() {
 	trans := Transactions{}
 	logrus.Infof("LastReportTime:%s", r.LastReportTime.String())
 	rst := r.Scanner.
-		DBEngine.Raw("SELECT COUNT(*) AS total, COUNT(CASE WHEN created_at > ? THEN 1 END) AS incr FROM transactions", r.LastReportTime).Scan(&trans)
+		DBEngine.Raw(`SELECT
+    COUNT(*) AS total,
+    COUNT(CASE WHEN created_at > ? THEN 1 END) AS incr,
+    SUM(fee) AS total_fee,
+    SUM(CASE WHEN created_at > ? THEN fee ELSE 0 END) AS incr_fee
+FROM
+    transactions`, r.LastReportTime, r.LastReportTime).(&trans)
 	if rst.Error != nil {
 		logrus.Errorln("get validatorNums error:", rst.Error)
 		return
@@ -49,6 +57,9 @@ func (r *ReportSpec) Run() {
 	}
 	diff := end - cursor.Slot
 	r.LastReportTime = now
-	logrus.WithField("Report", "true").Infof("balance:%s, total:%d, incr:%d, diff:%d",
-		balanceDecimal.Truncate(9), trans.Total, trans.Incr, diff)
+	totalFee, _ := decimal.NewFromString(trans.TotalFee)
+	incrFee, _ := decimal.NewFromString(trans.IncrFee)
+	logrus.WithField("Report", "true").Infof("balance:%s, total:%d[%s], incr:%d[%s], diff:%d",
+		balanceDecimal.Truncate(9), trans.Total, totalFee.Mul(decimal.New(1, -18)), trans.Incr,
+		incrFee.Mul(decimal.New(1, -18)), diff)
 }
